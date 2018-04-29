f43c8022200b0ecff48c30ecfd1fdbfa06e7be09
matias6942 <matias.zamora@ing.uchile.cl>
2018-04-28 23:35:44 -0300

    initPAge.html de la Tarea1

---
+++ show5Articles.php
@@ -0,0 +1,35 @@
+
+/**
+* Created by PhpStorm.
+* User: matias
+* Date: 4/28/18
+* Time: 11:23 PM
+*/
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reciclaje - Home</title>
+</head>
+<body>
+
+<h1>Esta Página es el Menú Inicial</h1>
+<h2>Listado con los 5 últimos artículos Ingresados</h2>
+
+//Tabla de 5 aticulos
+
+<?php
+echo "Wena Wena Compare"
+?>
+
+<br>
+<br>
+<form action="AddArticle.html">
+    <input type="submit" value="Informar Nuevo Artículo">
+</form>
+</body>
+</html>
+
+
+
