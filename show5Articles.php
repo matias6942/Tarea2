5133e698538605a0617a9a8388a5c623dda5788f
matias6942 <matias.zamora@ing.uchile.cl>
2018-04-30 21:19:30 -0300

    To Solve:
    Show "Comuna" and "Region" though a Join between database tables.
    Parse "Fecha"

--- show5Articles.php
+++ show5Articles.php
@@ -25,46 +25,76 @@
 
 $host = "127.0.0.1";
 $username = "client";
-$password = "";
-$db = "articleList";
+$password = "gYzlLqRJEQQi0j0E";
+$db = "tarea2";
 $port = "3306";
 
 // Create connection
-$conn = new mysqli($host, $username, $password);
+$conn = new mysqli($host, $username, $password, $db);
 
 // Check connection
 if ($conn -> connect_error){
-    die("Connection failed! ". $conn->connect_error);
+    die("Connection Failed ". $conn->connect_error);
 }
 
-echo "Connected Successfully";
-
-
+echo "Connected Successfully<br><br>";
 
 /**
+// Insert a row to the DB
+$sql =  "INSERT INTO articles VALUES ('2018-11-23', 'Pantalla Plasma', 'Metropolitana', 'Santiago Centro', 2, 3, '1lzamora2009@gmail.com')";
 
-$sql = "SELECT * FROM articles";
+if ($conn->query($sql) === TRUE){
+    echo "Values Inserted Succesfully";
+}
+
+else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close Connection
+$conn -> close();
+echo "Connection Closed<br>";
+*/
+
+
+echo "Starting Query...<br><br>";
+
+//$sql = "SELECT id, nombre, descripcion, fecha_ingreso, comuna_entrega, calle_numero, nombre_contacto, email_contacto, fono_contacto FROM articulo ORDER BY id DESC LIMIT 5";
+$sql = "SELECT * FROM articulo ORDER BY id DESC LIMIT 5";
 $result = $conn->query($sql);
 
-
 if ($result->num_rows > 0){
+
+    echo "<table style=\"width: 100%\">
+            <tr>
+                <th>Fecha de Ingreso</th>
+                <th>Artículo</th>
+                <th>Región</th>
+                <th>Comuna</th>
+                <th>N° Comentarios</th>
+                <th>N° Fotos</th>
+                <th>Email de Contacto</th>
+            </tr>";
+
+
     while ($row = $result->fetch_assoc()){
-        echo "<tr><td>" . $row["date"] . "</td><td>" . $row["article"] .
-            "<td></td>" . $row["region"] . "<td></td>" . $row["comuna"] .
-            "<td></td>" . $row["commentsQuantity"] . "<td></td>" . $row["photosQuantity"] .
-            "<td></td>" . $row["mail"];
+        echo "<tr><td>" . $row["fecha_ingreso"] . "</td><td>" . $row["nombre"] . "</td><td>" .
+            //"<td></td>" . $row["region"] . "<td></td>" . $row["comuna"] .
+            //"<td></td>" . $row["commentsQuantity"] . "<td></td>" . $row["photosQuantity"] .
+            "</td><td>" . $row["email_contacto"] . "</td></tr>";
     }
+
     echo "</table>";
 }
 
 else {
     echo "0 Results!";
 }
-$conn->close();
-echo "Finish!"
- */
-?>
 
+$conn->close();
+echo "<br><br>Finish!"
+
+?>
 
 
 <!--
