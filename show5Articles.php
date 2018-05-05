8ccad4972dd1b6b17a4ed0842d2eeffc9cc4774c
matias6942 <matias.zamora@ing.uchile.cl>
2018-05-05 01:47:50 -0300

    Undefined Variable counter

--- show5Articles.php
+++ show5Articles.php
@@ -24,11 +24,14 @@
 <?php
 
 /**
+ * Creates a connection to apache web server and returns the result of a query.
+ *
  * @param string $sql [required] SQL Query
- * @return An html table with fecha, articulo, region, comuna, comentarios, fotos y mail
+ * @return Query Result
+ *
  */
 
-function showQueryOnTable($sql){
+function queryResult($sql){
 
     $host = "127.0.0.1";
     $username = "client";
@@ -44,15 +47,54 @@ function showQueryOnTable($sql){
         die("Connection Failed ". $conn->connect_error);
     }
 
-    echo "Connected Successfully<br><br>";
-
-    echo "Starting Query...<br><br>";
+    echo "Connected Successfully to DataBase<br><br>";
 
     $result = $conn->query($sql);
+    $conn->close();
+    return $result;
+}
 
-    if ($result->num_rows > 0){
+/**
+ * @param $counter int Set the offset to show the 5 previous or the 5 next articles by id
+ */
 
-        echo "<table style=\"width: 100%\">
+function queryByIndex($counter){
+    $index = $counter*5;
+    $sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega,
+        comuna.nombre AS comuna_entrega, email_contacto,
+        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
+        FROM articulo, comuna, region
+        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
+        ORDER BY articulo.id DESC LIMIT 5," . $index;
+    return queryResult($sql);
+}
+
+if(isset($_POST["next5Articles"])){
+    $counter++;
+    $result = queryByIndex($counter);
+}
+
+elseif (isset($_POST["previous5Articles"])){
+    $counter--;
+    if ($counter < 0){
+        $counter = 0;
+    }
+    $result = queryByIndex($counter);
+}
+
+else{
+    $sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega, comuna.nombre AS comuna_entrega, email_contacto,
+        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
+        FROM articulo, comuna, region
+        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
+        ORDER BY articulo.id DESC LIMIT 5";
+    $counter = 0;
+    $result = queryResult($sql);
+}
+
+if ($result->num_rows > 0){
+
+    echo "<table style=\"width: 100%\">
             <tr>
                 <th>Fecha de Ingreso</th>
                 <th>Artículo</th>
@@ -64,38 +106,27 @@ function showQueryOnTable($sql){
             </tr>";
 
 
-        while ($row = $result->fetch_assoc()){
-            $fecha = preg_split( "/[\s]+/" ,$row["fecha_ingreso"])[0];
-            echo "<tr><td>" . $fecha . "</td><td>" . $row["articulo_nombre"] . "</td>" .
-                "<td>" . $row["region_entrega"] . "</td><td>" . $row["comuna_entrega"] . "</td>" .
-                "<td>" . $row["commentsQuantity"] . "</td><td>" . $row["photosQuantity"] . "</td>" .
-                "</td><td>" . $row["email_contacto"] . "</td></tr>";
-        }
-        echo "</table>";
+    while ($row = $result->fetch_assoc()){
+        $fecha = preg_split( "/[\s]+/" ,$row["fecha_ingreso"])[0];
+        echo "<tr><td>" . $fecha . "</td><td>" . $row["articulo_nombre"] . "</td>" .
+            "<td>" . $row["region_entrega"] . "</td><td>" . $row["comuna_entrega"] . "</td>" .
+            "<td>" . $row["commentsQuantity"] . "</td><td>" . $row["photosQuantity"] . "</td>" .
+            "</td><td>" . $row["email_contacto"] . "</td></tr>";
     }
-
-    else {
-        echo "0 Results!";
-    }
-
-    $conn->close();
-    echo "<br><br>Finish!";
-
+    echo "</table>";
 }
 
+else {
+    echo "0 Results!";
+}
 
+echo "<form action='show5Articles.php' method='post'>
+        <input type='submit' name='next5Articles' value='5 Siguientes'><br>
+</form>";
 
-$sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega, comuna.nombre AS comuna_entrega, email_contacto,
-        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
-        FROM articulo, comuna, region
-        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
-        ORDER BY articulo.id DESC LIMIT 5";
-
-showQueryOnTable($sql);
-
-
-
-
+echo "<form action='show5Articles.php' method='post'>
+        <input type='submit' name='previous5Articles' value='5 Anteriores'><br>
+</form>";
 
 ?>
 
---
+++ show5Articles2.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matias
+ * Date: 5/5/18
+ * Time: 1:01 AM
+ */
+
+
+/**
+ * @param string $sql [required] SQL Query
+ * @return An html table with fecha, articulo, region, comuna, comentarios, fotos y mail
+ */
+
+function showQueryOnTable($sql){
+
+    $host = "127.0.0.1";
+    $username = "client";
+    $password = "gYzlLqRJEQQi0j0E";
+    $db = "tarea2";
+    $port = "3306";
+
+// Create connection
+    $conn = new mysqli($host, $username, $password, $db);
+
+// Check connection
+    if ($conn -> connect_error){
+        die("Connection Failed ". $conn->connect_error);
+    }
+
+    echo "Connected Successfully<br><br>";
+
+    echo "Starting Query...<br><br>";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0){
+
+        echo "<table style=\"width: 100%\">
+            <tr>
+                <th>Fecha de Ingreso</th>
+                <th>Artículo</th>
+                <th>Región</th>
+                <th>Comuna</th>
+                <th>N° Comentarios</th>
+                <th>N° Fotos</th>
+                <th>Email de Contacto</th>
+            </tr>";
+
+
+        while ($row = $result->fetch_assoc()){
+            $fecha = preg_split( "/[\s]+/" ,$row["fecha_ingreso"])[0];
+            echo "<tr><td>" . $fecha . "</td><td>" . $row["articulo_nombre"] . "</td>" .
+                "<td>" . $row["region_entrega"] . "</td><td>" . $row["comuna_entrega"] . "</td>" .
+                "<td>" . $row["commentsQuantity"] . "</td><td>" . $row["photosQuantity"] . "</td>" .
+                "</td><td>" . $row["email_contacto"] . "</td></tr>";
+        }
+        echo "</table>";
+    }
+
+    else {
+        echo "0 Results!";
+    }
+
+    $conn->close();
+    echo "<br><br>Finish!";
+
+}
+
+/**
+ * @param $counter int Set the offset to show the 5 previous or the 5 next articles by id
+ */
+
+function queryByIndex($counter){
+    $index = $counter*5;
+    $sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega,
+        comuna.nombre AS comuna_entrega, email_contacto,
+        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
+        FROM articulo, comuna, region
+        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
+        ORDER BY articulo.id DESC LIMIT 5," . $index;
+    showQueryOnTable($sql);
+}
+
+$counter = 0;
+
+if($_POST["next5Articles"]){
+    $counter++;
+    queryByIndex($counter);
+}
+
+elseif ($_POST["previous5Articles"]){
+    $counter--;
+    queryByIndex($counter);
+}
+
+?>
