45c4445dda36f3e384d1e31d0f3b1413005f5bac
matias6942 <matias.zamora@ing.uchile.cl>
2018-05-05 00:52:41 -0300

    Table Working

--- show5Articles.php
+++ show5Articles.php
@@ -23,35 +23,36 @@
 <!--Tabla de 5 articulos-->
 <?php
 
-$host = "127.0.0.1";
-$username = "client";
-$password = "gYzlLqRJEQQi0j0E";
-$db = "tarea2";
-$port = "3306";
+/**
+ * @param string $sql [required] SQL Query
+ * @return An html table with fecha, articulo, region, comuna, comentarios, fotos y mail
+ */
+
+function showQueryOnTable($sql){
+
+    $host = "127.0.0.1";
+    $username = "client";
+    $password = "gYzlLqRJEQQi0j0E";
+    $db = "tarea2";
+    $port = "3306";
 
 // Create connection
-$conn = new mysqli($host, $username, $password, $db);
+    $conn = new mysqli($host, $username, $password, $db);
 
 // Check connection
-if ($conn -> connect_error){
-    die("Connection Failed ". $conn->connect_error);
-}
+    if ($conn -> connect_error){
+        die("Connection Failed ". $conn->connect_error);
+    }
 
-echo "Connected Successfully<br><br>";
+    echo "Connected Successfully<br><br>";
 
-echo "Starting Query...<br><br>";
+    echo "Starting Query...<br><br>";
 
-$sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega, comuna.nombre AS comuna_entrega, email_contacto,
-        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
-        FROM articulo, comuna, region
-        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
-        ORDER BY articulo.id DESC LIMIT 5";
+    $result = $conn->query($sql);
 
-$result = $conn->query($sql);
+    if ($result->num_rows > 0){
 
-if ($result->num_rows > 0){
-
-    echo "<table style=\"width: 100%\">
+        echo "<table style=\"width: 100%\">
             <tr>
                 <th>Fecha de Ingreso</th>
                 <th>Artículo</th>
@@ -63,22 +64,38 @@ if ($result->num_rows > 0){
             </tr>";
 
 
-    while ($row = $result->fetch_assoc()){
-        $fecha = preg_split( "/[\s]+/" ,$row["fecha_ingreso"])[0];
-        echo "<tr><td>" . $fecha . "</td><td>" . $row["articulo_nombre"] . "</td>" .
-            "<td>" . $row["region_entrega"] . "</td><td>" . $row["comuna_entrega"] . "</td>" .
-            "<td>" . $row["commentsQuantity"] . "</td><td>" . $row["photosQuantity"] . "</td>" .
-            "</td><td>" . $row["email_contacto"] . "</td></tr>";
+        while ($row = $result->fetch_assoc()){
+            $fecha = preg_split( "/[\s]+/" ,$row["fecha_ingreso"])[0];
+            echo "<tr><td>" . $fecha . "</td><td>" . $row["articulo_nombre"] . "</td>" .
+                "<td>" . $row["region_entrega"] . "</td><td>" . $row["comuna_entrega"] . "</td>" .
+                "<td>" . $row["commentsQuantity"] . "</td><td>" . $row["photosQuantity"] . "</td>" .
+                "</td><td>" . $row["email_contacto"] . "</td></tr>";
+        }
+        echo "</table>";
     }
-    echo "</table>";
+
+    else {
+        echo "0 Results!";
+    }
+
+    $conn->close();
+    echo "<br><br>Finish!";
+
 }
 
-else {
-    echo "0 Results!";
-}
 
-$conn->close();
-echo "<br><br>Finish!"
+
+$sql = "SELECT articulo.nombre AS articulo_nombre, fecha_ingreso, region.nombre AS region_entrega, comuna.nombre AS comuna_entrega, email_contacto,
+        (SELECT COUNT(comentario.comentario) FROM comentario) AS commentsQuantity, (SELECT COUNT(fotografia.id) FROM fotografia) AS photosQuantity
+        FROM articulo, comuna, region
+        WHERE articulo.comuna_id = comuna.id AND comuna.region_id = region.id
+        ORDER BY articulo.id DESC LIMIT 5";
+
+showQueryOnTable($sql);
+
+
+
+
 
 ?>
 
